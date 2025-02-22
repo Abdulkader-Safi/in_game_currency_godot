9583df2179e332f4c3bda91f884799a7f974a3b1
Abdulkader Safi <53955839+Abdulkader-Safi@users.noreply.github.com>
2025-02-22 17:26:55 +0300

    Add coin management to ShopWindow with increase button and label

--- Scripts/shop_window.gd
+++ Scripts/shop_window.gd
@@ -1,4 +1,15 @@
 extends Window
 
+var coins: int
+
+func _ready():
+	$CoinLabel.text = "$" + str(coins)
+	hide()
+
 func _on_close_requested():
 	hide()
+
+
+func _on_increase_coin_button_pressed():
+	coins += 100
+	$CoinLabel.text = "$" + str(coins)
